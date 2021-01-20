83f9aed014810fb4702864d33ebc2100c7d434c6
Erik <matovic.er@gmail.com>
2021-01-20 22:31:48 +0100

    Update getValidMoves

--- othello/OthelloGameLogic.cpp
+++ othello/OthelloGameLogic.cpp
@@ -12,40 +12,75 @@
 #include "OthelloGameLogic.hpp"
 
 // Checks possible move on Othello board by going up.
-int checkUp(const std::string& gameBoard, const size_t& diskIndex, const int& color)
+int checkUp(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk)
 {
 	if (diskIndex > 55)
 		return -1;
-	char opponentDisk = 'X';
-	if (!color) opponentDisk = 'O';
 
-	if (gameBoard[diskIndex] != opponentDisk && gameBoard[diskIndex] == '-')
+	if (gameBoard[diskIndex] != opponentDisk && gameBoard[diskIndex] != '-')
 		return -1;
 
 	if (gameBoard[diskIndex] == opponentDisk)
-		return checkUp(gameBoard, diskIndex + 8, color);
+		return checkUp(gameBoard, diskIndex - 8, opponentDisk);
 
 	return static_cast<int>(diskIndex);
 }
 
 /*
-std::vector<int> checkVertical(const std::string& gameBoard, const size_t& diskIndex, const int& color)
+std::vector<int> checkVertical(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk)
 {
 	std::vector<int> vectorDiskIndex;
-	if (gameBoard[diskIndex] > 7) checkDown(gameBoard, diskIndex, color);
-	if (gameBoard[diskIndex] < 56) checkUp(gameBoard, diskIndex, color);
+	if (gameBoard[diskIndex] > 7) checkDown(gameBoard, diskIndex, opponentDisk);
+	if (gameBoard[diskIndex] < 56) checkUp(gameBoard, diskIndex, opponentDisk);
 
 	return vectorDiskIndex;
 }*/
 
+// Checks all directions for possible moves on Othello board.
+std::vector<int> checkDirections(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk)
+{
+	std::vector<int> vectorValidMoves;// { static_cast<int>(diskIndex) };
+	gameBoard, diskIndex, opponentDisk;
+	int tmp = -1;
+	// go up
+	tmp = checkUp(gameBoard, diskIndex - 8, opponentDisk);
+	if (tmp != -1) vectorValidMoves.insert(vectorValidMoves.end(), tmp);
+
+
+	return vectorValidMoves;
+}
+
 // Gets all possible moves on Othello board.
-void getValidMoves(OthelloBot& othelloBot)
+std::vector<int> getValidMoves(OthelloBot& othelloBot)
 {
 	std::string possibleGameState = othelloBot.getGameState();
-	auto movePosition = possibleGameState.find('O');
-	possibleGameState[movePosition - 1] = '*';
-	std::cout << "FIND:" << movePosition << '\n';
-	std::cout << possibleGameState << '\n';
+	size_t pos = 0;
+	std::vector<int> vectorValidMoves;								// possible indexes of game state
+	char opponentDisk = 'X';
+	if (!othelloBot.getColor()) opponentDisk = 'O';
+	while (1)
+	{
+		size_t movePosition = possibleGameState.find(opponentDisk, pos);
+		if (movePosition == std::string::npos) break;
+
+		std::vector<int> tmp = checkDirections(possibleGameState, movePosition, 0);
+		vectorValidMoves.insert(vectorValidMoves.end(), tmp.begin(), tmp.end());
+		
+
+		pos = movePosition + 1;
+	}
+
+	for (int x : vectorValidMoves)
+	{
+		possibleGameState[x] = '*';
+		//std::cout << x << ' ' << opponentDisk;
+	}
+	std::cout << '\n';
+
+	//std::cout << "FIND:" << pos << '\n';
+	printGameState(possibleGameState);
+
+	return vectorValidMoves;
 }
 
 // Moves disk on Othello board based on given command.
@@ -53,4 +88,17 @@ void moveDisk(OthelloBot& othelloBot, const std::string& command)
 {
 	std::cout << command << '\n';
 	std::cout << '\n' << othelloBot << '\n';
+}
+
+
+// Writes current game state to output stream on a console.
+void printGameState(const std::string& gameState)
+{
+	std::cout << "  a b c d e f g h";
+	for (size_t i = 0, row = 0; i < 64; ++i)
+	{
+		if (i % 8 == 0)	std::cout << '\n' << ++row << ' ';
+		std::cout << gameState[i] << ' ';
+	}
+	std::cout << '\n';
 }
--- othello/OthelloGameLogic.hpp
+++ othello/OthelloGameLogic.hpp
@@ -26,73 +26,83 @@ static const std::string ROW = "12345678";
 * Checks possible move on Othello board by going up.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color 
+* @param opponentDisk represents current disk color 
 * @return integer value -1 for not possible move or free index on gameBoard
 */
-int checkUp(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+int checkUp(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 /**
 * Checks possible move on Othello board by going down.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color
+* @param opponentDisk represents current disk color
 * @return integer value -1 for not possible move or free index on gameBoard
 */
-int checkDown(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+int checkDown(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 
 /**
 * Checks possible move on Othello board by going left.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color
+* @param opponentDisk represents current disk color
 * @return integer value -1 for not possible move or free index on gameBoard
 */
-int checkLeft(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+int checkLeft(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 
 /**
 * Checks possible move on Othello board by going right.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color
+* @param opponentDisk represents current disk color
 * @return integer value -1 for not possible move or free index on gameBoard
 */
-int checkRight(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+int checkRight(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 /**
 * Checks all vertical moves on Othello board.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color
+* @param opponentDisk represents current disk color
 * @return vector of integers with all possible indexes on gameBoard
 */
-std::vector<int> checkVertical(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+//std::vector<int> checkVertical(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 /**
 * Checks all horizontal moves on Othello board.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color
+* @param opponentDisk represents current disk color
 * @return vector of integers with all possible indexes on gameBoard
 */
-std::vector<int> checkHorizontal(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+//std::vector<int> checkHorizontal(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 /**
 * Checks all diagonal moves on Othello board.
 * @param gameBoard represents board with O for being white disk and X being black disk
 * @param diskIndex represents index on game board to be checked
-* @param color represents current disk color
+* @param opponentDisk represents current disk color
 * @return vector of integers with all possible indexes on gameBoard
 */
-std::vector<int> checkDiagonal(const std::string& gameBoard, const size_t& diskIndex, const int& color);
+//std::vector<int> checkDiagonal(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
+
+
+/**
+* Checks all directions for possible moves on Othello board.
+* @param gameBoard represents board with O for being white disk and X being black disk
+* @param diskIndex represents index on game board to be checked
+* @param opponentDisk represents current disk color
+* @return vector of integers with all possible indexes on gameBoard
+*/
+std::vector<int> checkDirections(const std::string& gameBoard, const size_t& diskIndex, const char& opponentDisk);
 
 /**
 * Gets all possible moves on Othello board.
 * @param othelloBot object of class OthelloBot
-* @return 
+* @return vector of valid indexes representing valid moves on Othello board
 */
-void getValidMoves(OthelloBot& othelloBot);
+std::vector<int> getValidMoves(OthelloBot& othelloBot);
 
 /**
 * Moves disk on Othello board based on given command.
@@ -101,3 +111,9 @@ void getValidMoves(OthelloBot& othelloBot);
 * @return
 */
 void moveDisk(OthelloBot& othelloBot, const std::string& command);
+
+/**
+* Writes current game state to output stream on a console.
+* @param gameState represents current Othello board with O for being white disk and X being black disk
+*/
+void printGameState(const std::string& gameState);
